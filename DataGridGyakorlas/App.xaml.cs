fd4000e123262a61483a7f87f89810b81ad8c914
Csongoranevem <czakocsongor@turr.hu>
2025-04-04 09:35:36 +0200

    mindegyAdd project files.

---
+++ DataGridGyakorlas/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DataGridGyakorlas
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static string dataBaseName = "Betegek.db";
+        static string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        public static string dataBasePath = System.IO.Path.Combine(folderPath, dataBaseName);
+
+    }
+
+}
