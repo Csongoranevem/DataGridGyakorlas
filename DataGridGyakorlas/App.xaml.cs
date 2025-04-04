7ea6fd9582789518c2ac9e2286388365a02c7cb7
Csongoranevem <czakocsongor@turr.hu>
2025-04-04 09:35:36 +0200

    Add project files.

---
+++ DataGridGyakorlas/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DataGridGyakorlas
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
