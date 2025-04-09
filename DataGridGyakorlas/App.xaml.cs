84c471d9e6abb91ad6a4d55fd242064bb826dd9a
Csongoranevem <czakocsongor@turr.hu>
2025-04-09 10:57:06 +0200

    elso commit

--- DataGridGyakorlas/App.xaml.cs
+++ DataGridGyakorlas/App.xaml.cs
@@ -9,11 +9,14 @@ namespace DataGridGyakorlas
     /// </summary>
     public partial class App : Application
     {
+//<<<<<<< HEAD
         static string dataBaseName = "Betegek.db";
         static string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
         public static string dataBasePath = System.IO.Path.Combine(folderPath, dataBaseName);
 
+//=======
+//>>>>>>> 7ea6fd9582789518c2ac9e2286388365a02c7cb7
     }
 
 }
